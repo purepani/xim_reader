823177d8fbd01d5c13d5627a9595cc6f4dab7f69
purepani <pani0028@umn.edu>
2025-02-22 22:35:51 -0600

    Adds typing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 mod reader;
+use pyo3_stub_gen::define_stub_info_gatherer;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -10,6 +11,11 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn xim_reader(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<reader::XIMImage>();
+    let _ = m.add_class::<reader::XIMImage>();
+    let _ = m.add_class::<reader::XIMHeader>();
+    let _ = m.add_class::<reader::XIMHistogram>();
+    let _ = m.add_class::<reader::XIMProperties>();
     Ok(())
 }
+
+define_stub_info_gatherer!(stub_info);
